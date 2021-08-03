78ba633f3203b223da47f62b5f4a3392008929bd
cmbngs <cmbngs@frd123-r008759.local>
2021-08-02 22:09:24 -0400

    add trimming

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 cd 01-errorcorrecting
 docker build -t 01-errorcorrecting .
 cd ..
+
+cd 02-trimming
+docker build -t 02-trimming .
+cd ..
--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
 docker run -v /Users/cmbngs/Desktop/Mika/SParvustranscriptome/files:/files 01-errorcorrecting
+docker run -v /Users/cmbngs/Desktop/Mika/SParvustranscriptome/files:/files 02-trimming
